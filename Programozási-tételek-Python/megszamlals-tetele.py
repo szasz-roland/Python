63ae2eee5b57b06e40f660c54ef73947e1138dc4
szasz.roland <szaszroland11@gmail.com>
2022-01-19 08:34:05 +0100

    commit

---
+++ Programozási-tételek-Python/megszamlals-tetele.py
@@ -0,0 +1,8 @@
+lista = [3, 8, 2, 4, 5, 1, 6]
+
+count = 0
+for num in lista:
+    if num > 5:
+        count += 1
+
+print("5-nél nagyobb: ", count)
---
+++ Programozási-tételek-Python/osszegzes-tetele.py
@@ -0,0 +1,7 @@
+lista = [3, 8, 2, 4, 5, 1, 6]
+
+osszeg = 0
+for num in lista:
+    osszeg += num
+
+print("Összeg: ", osszeg)
